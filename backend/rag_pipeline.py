7b33691ca648b8012b6177c24f1412d7bb77faaf
snehagurjar <snehagurjar0901@gmail.com>
2026-04-12 16:06:13 +0530

    fixed langchain import error

--- backend/rag_pipeline.py
+++ backend/rag_pipeline.py
@@ -2,7 +2,7 @@
 from PyPDF2 import PdfReader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.llms import HuggingFacePipeline
 
 # ✅ Stable LLM (longer answers)
