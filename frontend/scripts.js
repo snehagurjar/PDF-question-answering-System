6e0a4aef573e0490715344b7b75458be6a37ff53
snehagurjar <snehagurjar0901@gmail.com>
2026-04-12 18:05:45 +0530

    updated backend url for deployment

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,6 +1,5 @@
 
-const BASE_URL = "http://127.0.0.1:5000";
-
+const BASE_URL = "https://pdf-question-answering-system.onrender.com";
 // const chatBox = document.getElementById("chat-box");
 let chatBox;
 
