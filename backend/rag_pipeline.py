e91d11e2776fd0983aee987546fe5ab27b6f5fb5
snehagurjar <snehagurjar0901@gmail.com>
2026-04-12 17:21:53 +0530

    removed heavy model to fix memory issue

--- backend/rag_pipeline.py
+++ backend/rag_pipeline.py
@@ -36,7 +36,9 @@ def process_pdf(filepath):
 
     chunks = splitter.create_documents([text])
 
-    embeddings = HuggingFaceEmbeddings()
+    embeddings = HuggingFaceEmbeddings(
+        model_name="all-MiniLM-L6-v2"
+    )
     vector_store = FAISS.from_documents(chunks, embeddings)
 
     return vector_store.as_retriever(search_kwargs={"k": 4})  # 🔥 more chunks
